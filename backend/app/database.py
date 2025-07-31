a3e249afd8fe20dc4d49a6ea4c78e16d5c7679d1
Monica Dhieu <m.dhieu@alustudent.com>
2025-07-31 23:20:41 +0200

    Create database.py

---
+++ backend/app/database.py
@@ -0,0 +1,8 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+
+DATABASE_URL = "sqlite:///./app.db"  
+database = Database(DATABASE_URL)
+metadata = MetaData()
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
