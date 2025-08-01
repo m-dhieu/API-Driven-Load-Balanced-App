25ea58bd2b74f9bae10ea1112287266d5cd74485
Monica Dhieu <m.dhieu@alustudent.com>
2025-08-01 09:25:52 +0200

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,11 @@
 from databases import Database
 from sqlalchemy import create_engine, MetaData
 
-DATABASE_URL = "sqlite:///./app.db"  
+DATABASE_URL = "sqlite:///./app.db"  # relative path in project root
+
 database = Database(DATABASE_URL)
 metadata = MetaData()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}  # for SQLite
+)
